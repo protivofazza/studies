26055335299781a949aa93576902058f9e067e22
protivofazza <59832106+protivofazza@users.noreply.github.com>
2020-02-04 22:37:24 +0200

    Update hw_3_1.py

--- hw_3_1.py
+++ hw_3_1.py
@@ -3,13 +3,8 @@ from string import whitespace, punctuation
 print("Завдання №3.1.\nПрограма підрахунку слів у тексті")
 print("_" * 33, "\nЦя програма приймає введені користувачем тексти, які складються зі слів,\nформує словник із "
                 "статистикою введених слів і виводить цей словник на екран\nу вигляді форматованої таблиці."
-                "\n\nПам'ятайте, що для припинення роботи програми необхідно ввести пустий рядок.\n"
+                "\n\nПам'ятайте, що для припинення введення необхідно ввести пустий рядок.\n"
                 "Давайте розпочнемо...")
-
-# Пам'ятайте, що для припинення роботи програми необхідно ввести пустий рядок - пустий рядок - будь-що
-# print('whitespace = {}'.format(repr(whitespace)))
-# print('punctuation = {}'.format(repr(punctuation)))
-# print('\n', punctuation)
 
 
 def text_input(string):    # Ф-ія видаляє знаки пунктуації із введеного тексту (крім дефісу всередині слів та апострофу)
@@ -27,12 +22,6 @@ def check_exit():
         return False
     else:
         return True
-
-    # for word in words:
-    #     if punctuation in word:
-    #         word_1 = word[-1:]
-    #     word_1 = word.lower()
-    #     print(word_1, type(word_1))
 
 
 radio_button = True
