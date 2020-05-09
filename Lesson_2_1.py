79b546e194d8b5a9bd413232871f7d9f67b410ba
Protivofazza <protivofazza@outlook.com>
2020-05-09 12:31:56 +0300

    Final changes

--- Lesson_2_1.py
+++ Lesson_2_1.py
@@ -15,15 +15,34 @@ class Vehicle:
     def set_maximum_mass(self, mass):
         self.maximum_mass = mass
 
+    def indicate_features(self):
+        print('No features')
+
 
 class Truck(Vehicle):
     def __init__(self, model, vin, maximum_mass, lift_capacity):
-        self.model = model
-        self.vin = vin
-        self.maximum_mass = maximum_mass
+        super().__init__(model, vin, maximum_mass)
         self.lift_capacity = lift_capacity
+
+    def indicate_features(self):
+        print('It is possible to attach a trailer')
+
+    def __str__(self):
+        return f"Модель автомобіля: {self.model}, індивідуальний код кузова: {self.vin}, максимальна дозволена маса: " \
+               f"{self.maximum_mass} кг, максимальний вантаж підйомника: {self.lift_capacity} кг, країна реєстрації: " \
+               f"{Truck.country_of_registration}"
 
 
 class Car(Vehicle):
-    def set_seats(self, value):
-        self.number_of_seats = value
+    def __init__(self, model, vin, maximum_mass, pass_seats):
+        super().__init__(model, vin, maximum_mass)
+        self.pass_seats = pass_seats
+
+    def indicate_features(self):
+        print("Ви можете попередньо вибрати колір екстер'єру та інтер'єру авто ")
+
+
+truck_01 = Truck('Volvo', '2464VCD005678', 22000, 3500)
+print(truck_01.model, truck_01.vin, truck_01.maximum_mass, truck_01.lift_capacity)
+
+print(truck_01)
